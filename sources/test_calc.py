a39ce482594ebb105e50ed55473e761e7ad17434
Skypeblue <fredericfrancine@gmail.com>
2022-09-16 15:38:45 +0200

    fix unit tests

--- sources/test_calc.py
+++ sources/test_calc.py
@@ -1,5 +1,6 @@
-import unittest 
-import calc 
+import unittest
+import calc
+
 
 class Test(unittest.TestCase):
     """
@@ -10,103 +11,103 @@ class Test(unittest.TestCase):
         """
         L'addition entre 2 et 6 doit donner 8.
         """
-        self.assertEqual(calc.add("2","7"),8)    
+        self.assertEqual(calc.add("2", "6"), 8)
 
     def test_add_str(self):
         """
         L'addition entre 2 et add ne doit rien retourner.
         """
-        self.assertIsNone(calc.add("2","add"))    
+        self.assertIsNone(calc.add("2", "add"))
 
     def test_add_float(self):
         """
         L'addition entre 2 float ne doit rien retourner.
         """
-        self.assertIsNone(calc.add("2.2","4.5"))
+        self.assertIsNone(calc.add("2.2", "4.5"))
 
     def test_sous(self):
         """
         La soustraction de 144 par 12 doit retourner 132.
         """
-        self.assertEqual(calc.sous("144","12"),132)
+        self.assertEqual(calc.sous("144", "12"), 132)
 
     def test_sous_str(self):
         """
         La soustraction de 31 par - ne doit rien retourner.
         """
-        self.assertIsNone(calc.sous("31","-"))
+        self.assertIsNone(calc.sous("31", "-"))
 
     def test_modulo(self):
         """
         Le résultat de 8 modulo 2 est 0
         """
-        self.assertEqual(calc.modulo(8,2),0)
+        self.assertEqual(calc.modulo(8, 2), 0)
 
     def test_modulo_str(self):
         """
         Le résultat de 8 modulo da ne doit rien retourner
         """
-        self.assertIsNone(calc.modulo(8,"da"))
-    
-    def test_sous_float(self):        
+        self.assertIsNone(calc.modulo(8, "da"))
+
+    def test_sous_float(self):
         """
         La soustraction entre 2 float ne doit rien retourner.
         """
-        self.assertIsNone(calc.sous("31.12","42.1"))
+        self.assertIsNone(calc.sous("31.12", "42.1"))
 
     def test_mult(self):
         """
         La multiplication de 13 par 13 doit retourner 169.
         """
-        self.assertEqual(calc.mult("13","13"),169)
+        self.assertEqual(calc.mult("13", "13"), 169)
 
     def test_mult_str(self):
         """
         La multiplication entre mult et 18 ne doit rien retourner.
         """
-        self.assertIsNone(calc.mult("mult","18"))
+        self.assertIsNone(calc.mult("mult", "18"))
 
     def test_mult_float(self):
         """
         La multiplication entre 2 float ne doit rien retourner.
         """
-        self.assertIsNone(calc.sous("0.5","2.8"))
+        self.assertIsNone(calc.sous("0.5", "2.8"))
 
     def test_div(self):
         """
         La division de 12 par 2 doit retourner 6.
         """
-        self.assertEqual(calc.div("12","2"),6)
+        self.assertEqual(calc.div("12", "2"), 6)
 
     def test_div_str(self):
         """
         La division de douze par deux ne doit rien retourner.
         """
-        self.assertIsNone(calc.div("douze","deux"))
+        self.assertIsNone(calc.div("douze", "deux"))
 
     def test_div_float(self):
         """
         La division entre 2 float ne doit rien retourner.
         """
-        self.assertIsNone(calc.div("31.0","7.0"))
+        self.assertIsNone(calc.div("31.0", "7.0"))
 
     def test_div_zero(self):
         """
         Une division par 0.
         """
-        self.assertIsNone(calc.div("2","0"))
+        self.assertIsNone(calc.div("2", "0"))
 
     def test_ope(self):
-        """        
+        """
         La soustraction entre 12 et 2 doit retourner 10.
         """
-        self.assertEqual(calc.sous("12","2"),10)
+        self.assertEqual(calc.sous("12", "2"), 10)
 
     def test_ope_erreur(self):
         """
         L'opérateur factoriel n'est pas pris en compte.
         """
-        self.assertIsNone(calc.ope("!","2","3"))
+        self.assertIsNone(calc.ope("!", "2", "3"))
 
-    if __name__ == '__main__':
+    if __name__ == "__main__":
         unittest.main()
