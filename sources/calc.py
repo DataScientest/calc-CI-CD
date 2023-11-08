d180b7d869f67dc584643b2a0d2c7822b3b5f96f
MHachani <meha.hachani@gmail.com>
2023-11-08 20:44:23 +0100

    Second change

--- sources/calc.py
+++ sources/calc.py
@@ -6,7 +6,7 @@ def add(arg1,arg2):
     try:
         return int(arg1)+int(arg2)
     except ValueError: 
-        print("Un des arguments n'est pas un entier.") 
+        print("Vous devez entrer un entier.") 
 
 def sous(arg1,arg2):
     try:
